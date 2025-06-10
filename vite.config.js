901fd1e9129e5efa9598d5a05852a3335a7cecaa
stefanpalsson <stefanpalsson86@gmail.com>
2025-06-10 11:21:30 +0200

    Fix lang for Lia

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: "https://github.com/StefanPalsson/Stefan-CV.git", // ← sätt detta till repo-namnet på GitHub
   plugins: [react()],
-})
+});
